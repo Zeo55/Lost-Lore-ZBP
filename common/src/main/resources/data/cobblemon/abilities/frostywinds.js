b570250b122bc09a4ea5e7424c4ea90d845361af
Lvnatic <thomaslinnemann55@gmail.com>
2026-03-08 12:56:59 +0100

    Updated Snowman Munchlax/Snorlax, added frosty winds ability (custom)

---
+++ common/src/main/resources/data/cobblemon/abilities/frostywinds.js
@@ -0,0 +1,18 @@
+{
+	onBasePowerPriority: 19,
+	onBasePower(basePower, attacker, defender, move) {
+		if (move.flags['wind']) {
+			return this.chainModify(1.5);
+		}
+	},
+	onAnyModifyAccuracyPriority: -1,
+    onAnyModifyAccuracy(accuracy, target, source) {
+    	if (move.flags['wind']) {
+    		return this.chainModify([4506, 4096]);
+    	}
+    }
+	flags: {},
+	name: "Frosty Winds",
+	rating: 3.5,
+	num: 9980,
+}

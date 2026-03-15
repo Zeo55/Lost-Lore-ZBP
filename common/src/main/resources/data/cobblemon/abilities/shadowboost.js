52664fae8e9f11f1b41ee5f0d9f8fe14348b5451
Lvnatic <thomaslinnemann55@gmail.com>
2026-03-15 22:33:34 +0100

    Fixed Shadow Mewtwo's Shadow Boost ability setting all non-shadow moves to 0 damage

--- common/src/main/resources/data/cobblemon/abilities/shadowboost.js
+++ common/src/main/resources/data/cobblemon/abilities/shadowboost.js
@@ -1,16 +1,20 @@
 {
-	onModifyDamage(damage, source, target, move) {
-		let modifier = [4096, 4096];
-		if (move && target.getMoveHitData(move).typeMod > 0) {
-			modifier = this.chainModify([5120, 4096]);
-		}
-		if (move && move.type === "Psychic") {
-			this.chainModify([6144, 4096]);
-		}
-		return modifier;
-	},
-	flags: {},
-	name: "Shadow Boost",
-	rating: 5.0,
-	num: 9999,
+    onBasePowerPriority: 20,
+    onBasePower(basePower, source, target, move) {
+        if (!move || move.category === 'Status') return;
+        let mod = 1;
+        if (target.getMoveHitData(move).typeMod > 0) {
+            mod *= 1.25;
+        }
+        if (move.type === "Psychic") {
+            mod *= 1.5;
+        }
+        if (mod !== 1) {
+            return this.chainModify(mod);
+        }
+    },
+    flags: {},
+    name: "Shadow Boost",
+    rating: 5.0,
+    num: 9999,
 }

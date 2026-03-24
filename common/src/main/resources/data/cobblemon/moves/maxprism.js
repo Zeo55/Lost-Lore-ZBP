898a0424881775a39f9db904b418137b581ab195
Lvnatic <thomaslinnemann55@gmail.com>
2026-03-24 19:54:52 +0100

    Added Glinting Beam (Z) & Max Prism (Gmax) + lang

---
+++ common/src/main/resources/data/cobblemon/moves/maxprism.js
@@ -0,0 +1,23 @@
+{
+	num: 10101,
+	accuracy: true,
+	basePower: 10,
+	category: "Physical",
+	isNonstandard: "Past",
+	name: "Max Void",
+	pp: 10,
+	priority: 0,
+	flags: {},
+	isMax: true,
+	self: {
+		onHit(source) {
+			if (!source.volatiles['dynamax']) return;
+			for (const pokemon of source.alliesAndSelf()) {
+				this.boost({def: 1, spd: 1}, pokemon);
+			}
+		},
+	},
+	target: "adjacentFoe",
+	type: "Crystal",
+	contestType: "Cool",
+}

884b53905ecdb925c275b733d5f55abe161be160
Lvnatic <thomaslinnemann55@gmail.com>
2026-03-25 02:43:41 +0100

    Added 4 crystal moves

---
+++ common/src/main/resources/data/cobblemon/moves/crystalcannon.js
@@ -0,0 +1,17 @@
+{
+	num: 10103,
+	accuracy: 100,
+	basePower: 80,
+	category: "Special",
+	name: "Crystal Cannon",
+	pp: 10,
+	priority: 0,
+	flags: {protect: 1, mirror: 1, metronome: 1},
+	secondary: {
+		chance: 15,
+		status: 'brn',
+	},
+	target: "normal",
+	type: "Crystal",
+	contestType: "Beautiful",
+}
---
+++ common/src/main/resources/data/cobblemon/moves/unstablesurge.js
@@ -0,0 +1,18 @@
+{
+	num: 10105,
+	accuracy: 90,
+	basePower: 130,
+	category: "Special",
+	name: "Unstable Surge",
+	pp: 10,
+	priority: 0,
+	flags: {protect: 1, mirror: 1, gravity: 1, metronome: 1},
+	hasCrashDamage: true,
+	onMoveFail(target, source, move) {
+		this.damage(source.baseMaxhp / 2, source, source, this.dex.conditions.get('Unstable Surge'));
+	},
+	secondary: null,
+	target: "normal",
+	type: "Crystal",
+	contestType: "Beautiful",
+}

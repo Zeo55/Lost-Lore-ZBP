8ecb76431a60f4974c989d13b6acfd2a0550c0d5
Lvnatic <thomaslinnemann55@gmail.com>
2026-03-25 19:54:28 +0100

    Added 3 more crystal moves

---
+++ common/src/main/resources/data/cobblemon/moves/crystalbarrier.js
@@ -0,0 +1,19 @@
+{
+	num: 10106,
+	accuracy: true,
+	basePower: 0,
+	category: "Status",
+	name: "Crystal Barrier",
+	pp: 15,
+	priority: 0,
+	flags: {snatch: 1, metronome: 1},
+	boosts: {
+		def: 1,
+		spd: -1,
+	},
+	secondary: null,
+	target: "self",
+	type: "Crystal",
+	zMove: {effect: 'clearnegativeboost'},
+	contestType: "Tough",
+}
---
+++ common/src/main/resources/data/cobblemon/moves/shinyglare.js
@@ -0,0 +1,18 @@
+{
+	num: 10108,
+	accuracy: true,
+	basePower: 0,
+	category: "Status",
+	name: "Shiny Glare",
+	pp: 5,
+	priority: 0,
+	flags: {reflectable: 1, mirror: 1, metronome: 1},
+	onHit(target, source, move) {
+		return target.addVolatile('trapped', source, move, 'trapper');
+	},
+	secondary: null,
+	target: "normal",
+	type: "Crystal",
+	zMove: {boost: {spd: 1}},
+	contestType: "Beautiful",
+}
